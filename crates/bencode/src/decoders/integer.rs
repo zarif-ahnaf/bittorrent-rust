90cbc6fa8dc4acfe05cfda9e88aab68db43bf208
zarif-ahnaf <86199070+zarif-ahnaf@users.noreply.github.com>
2025-11-22 09:43:17 +0600

    yes

--- crates/bencode/src/decoders/integer.rs
+++ crates/bencode/src/decoders/integer.rs
@@ -20,6 +20,14 @@ pub fn decode_integer(data: &[u8]) -> Result<(isize, &[u8]), &'static str> {
     if int_bytes.len() > 1 && int_bytes[0] == b'0' {
         return Err("Leading zeros are not allowed");
     }
+    // Negative zero check
+    if int_bytes[0] == b'-' && int_bytes[1] == b'0' {
+        return Err("Negative leading zeros are not allowed");
+    }
+
+    // println!("{:?}", int_bytes.len());
+    // println!("{:?} {:?}", int_bytes[0], int_bytes[1]);
+    // println!("{:?} {:?}", b'-', b'0');
 
     // Convert to string and parse
     let int_str = std::str::from_utf8(int_bytes).map_err(|_| "Invalid UTF-8")?;
